a6136e1f07631431a9b2b5f540872608dc3742a5
SirajKarim <msirajakarim437@gmail.com>
2020-08-25 08:03:08 +0500

    Connect redux with component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { connect } from "react-redux";
+import store from "./basic";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log(this.props);
+    store.dispatch({
+      type: "ADD TODO",
+    });
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log("ComponentWillRecieveProps", nextProps);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStatetoProps(state) {
+  return {
+    Todo: state.Todo.text,
+  };
+}
+export default connect(mapStatetoProps, null)(App);
